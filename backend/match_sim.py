66fd94acbb5dfa533af5cf2c6b1dd94284060934
Tom Joshi-Cale <tj294@exeter.ac.uk>
2025-08-28 19:14:52 +0100

    Fix ball_update when matchPlayed=1

--- backend/match_sim.py
+++ backend/match_sim.py
@@ -167,120 +167,120 @@ async def ball_update(matchID):
         #         "awayPlayers": awayTeamPlayers,
         #         "date": info['scheduledDate']
         #     }
-        # elif info['matchPlayed'] == 1:
-        #     # Needs refining to only necessary information
-        #     homeTeamName = cur.execute("SELECT name FROM teams WHERE teamID = ?;", (int(info['homeTeamID']),)).fetchone()['name']
-        #     awayTeamName = cur.execute("SELECT name FROM teams WHERE teamID = ?", (info['awayTeamID'],)).fetchone()['name']
-        #     if info['batFirstID'] == info['homeTeamID']:
-        #         batFirstName = homeTeamName
-        #         batSecondID = info['awayTeamID']
-        #         batSecondName = awayTeamName
-        #     else:
-        #         batFirstName = awayTeamName
-        #         batSecondID = info['homeTeamID']
-        #         batSecondName = homeTeamName
-        #     if info['bFOvers'] == '19.6':
-        #         info['bFOvers'] = '20.0'
-        #     # Calculate Run Rate
-        #     if type(info['bFOvers']) == float:
-        #             balls_faced = 0
-        #     else:
-        #             balls_faced = int(info['bFOvers'].split('.')[0]) * 6 + int(info['bFOvers'].split('.')[1])
-        #     runs_scored = info['bFRuns']
-        #     if balls_faced == 0:
-        #             bfCRR = 0.00
-        #     else:
-        #             bfCRR = runs_scored / (int(balls_faced) / 6)
-        #     if info['bSOvers'] == "YTB":
-        #         bsCRR = 0.00
-        #         bsRRR = 0.00
-        #     else:
-        #         balls_faced = int(info['bSOvers'].split('.')[0]) * 6 + int(info['bSOvers'].split('.')[1])
-        #         balls_remaining = 120-balls_faced
-        #         bsCRR = info['bSRuns'] / (balls_faced/6)
-        #         if balls_remaining > 0:
-        #             bsRRR = (info['bFRuns'] - info['bSRuns']) / (balls_remaining/6)
-        #         else:
-        #             bsRRR = 0
-        #     if info['sbattingCard'] == None:
-        #         sbattingCard = None
-        #         syetToBat = None
-        #         sbowlingCard = None
-        #     else:
-        #         sbattingCard = json.loads(info['sbattingCard'])
-        #         syetToBat = json.loads(info['syetToBat'])
-        #         sbowlingCard = json.loads(info['sbowlingCard'])
-        #     # logText = json.loads(info['log'])
-        #     logText = []
-        #     for log in json.loads(info['log']):
-        #             logText.append(json.loads(log))
-        #     liveInfo = {
-        #         "status": "played",
-        #         "season": info['season'],
-        #         "match": info['matchNo'],
-        #         "homeTeam": homeTeamName,
-        #         "homeTeamID": info['homeTeamID'],
-        #         "awayTeam": awayTeamName,
-        #         "awayTeamID": info['awayTeamID'],
-        #         "bfName": batFirstName,
-        #         "bfID": info['batFirstID'],
-        #         "bsName": batSecondName,
-        #         "bsID": batSecondID,
-        #         "bfRuns": info['bFRuns'],
-        #         "bfWickets": info['bFWickets'],
-        #         "bfOvers": info['bFOvers'],
-        #         "bfCRR": bfCRR,
-        #         "bsRuns": info['bSRuns'],
-        #         "bsWickets": info['bSWickets'],
-        #         "bsOvers": info['bSOvers'],
-        #         "bsCRR": bsCRR,
-        #         "bsRRR": bsRRR,
-        #         "overResults": info['overResults'],
-        #         "ballEvent": info['ballEvent'],
-        #         "ballText": info['ballText'],
-        #         "currentOver": info['currentOver'],
-        #         "currentBall": info['currentBall'],
-        #         "strikeID": info['batter1ID'],
-        #         "strikeFName": info['batter1FName'],
-        #         "strikeLName": info['batter1LName'],
-        #         "strikeRuns": info['batter1Runs'],
-        #         "strikeBalls": info['batter1Balls'],
-        #         "strikeFours": info['batter1Fours'],
-        #         "strikeSixes": info['batter1Sixes'],
-        #         "strikeSR": info['batter1SR'],
-        #         "nstrikeID": info['batter2ID'],
-        #         "nstrikeFName": info['batter2FName'],
-        #         "nstrikeLName": info['batter2LName'],
-        #         "nstrikeRuns": info['batter2Runs'],
-        #         "nstrikeBalls": info['batter2Balls'],
-        #         "nstrikeFours": info['batter2Fours'],
-        #         "nstrikeSixes": info['batter2Sixes'],
-        #         "nstrikeSR": info['batter2SR'],
-        #         "sbowlID": info['bowler1ID'],
-        #         "sbowlFName": info['bowler1FName'],
-        #         "sbowlLName": info['bowler1LName'],
-        #         "sbowlOvers": info['bowler1Overs'],
-        #         "sbowlMaidens": info['bowler1Maidens'],
-        #         "sbowlRuns": info['bowler1Runs'],
-        #         "sbowlWickets": info['bowler1Wickets'],
-        #         "sbowlEcon": info['bowler1Econ'],
-        #         "nsbowlID": info['bowler2ID'],
-        #         "nsbowlFName": info['bowler2FName'],
-        #         "nsbowlLName": info['bowler2LName'],
-        #         "nsbowlOvers": info['bowler2Overs'],
-        #         "nsbowlMaidens": info['bowler2Maidens'],
-        #         "nsbowlRuns": info['bowler2Runs'],
-        #         "nsbowlWickets": info['bowler2Wickets'],
-        #         "nsbowlEcon": info['bowler2Econ'],
-        #         "lBat": info['lastBat'],
-        #         "fbattingCard": json.loads(info['fbattingCard']),
-        #         "fyetToBat": json.loads(info['fyetToBat']),
-        #         "fbowlingCard": json.loads(info['fbowlingCard']),
-        #         "sbattingCard": sbattingCard,
-        #         "syetToBat": syetToBat,
-        #         "sbowlingCard": sbowlingCard,
-        #         "log": logText
-        #     }
+        elif info['matchPlayed'] == 1:
+            # Needs refining to only necessary information
+            homeTeamName = cur.execute("SELECT name FROM teams WHERE teamID = ?;", (int(info['homeTeamID']),)).fetchone()['name']
+            awayTeamName = cur.execute("SELECT name FROM teams WHERE teamID = ?", (info['awayTeamID'],)).fetchone()['name']
+            if info['batFirstID'] == info['homeTeamID']:
+                batFirstName = homeTeamName
+                batSecondID = info['awayTeamID']
+                batSecondName = awayTeamName
+            else:
+                batFirstName = awayTeamName
+                batSecondID = info['homeTeamID']
+                batSecondName = homeTeamName
+            if info['bFOvers'] == '19.6':
+                info['bFOvers'] = '20.0'
+            # Calculate Run Rate
+            if type(info['bFOvers']) == float:
+                    balls_faced = 0
+            else:
+                    balls_faced = int(info['bFOvers'].split('.')[0]) * 6 + int(info['bFOvers'].split('.')[1])
+            runs_scored = info['bFRuns']
+            if balls_faced == 0:
+                    bfCRR = 0.00
+            else:
+                    bfCRR = runs_scored / (int(balls_faced) / 6)
+            if info['bSOvers'] == "YTB":
+                bsCRR = 0.00
+                bsRRR = 0.00
+            else:
+                balls_faced = int(info['bSOvers'].split('.')[0]) * 6 + int(info['bSOvers'].split('.')[1])
+                balls_remaining = 120-balls_faced
+                bsCRR = info['bSRuns'] / (balls_faced/6)
+                if balls_remaining > 0:
+                    bsRRR = (info['bFRuns'] - info['bSRuns']) / (balls_remaining/6)
+                else:
+                    bsRRR = 0
+            if info['sbattingCard'] == None:
+                sbattingCard = None
+                syetToBat = None
+                sbowlingCard = None
+            else:
+                sbattingCard = json.loads(info['sbattingCard'])
+                syetToBat = json.loads(info['syetToBat'])
+                sbowlingCard = json.loads(info['sbowlingCard'])
+            # logText = json.loads(info['log'])
+            logText = []
+            for log in json.loads(info['log']):
+                    logText.append(json.loads(log))
+            liveInfo = {
+                "status": "played",
+                "season": info['season'],
+                "match": info['matchNo'],
+                "homeTeam": homeTeamName,
+                "homeTeamID": info['homeTeamID'],
+                "awayTeam": awayTeamName,
+                "awayTeamID": info['awayTeamID'],
+                "bfName": batFirstName,
+                "bfID": info['batFirstID'],
+                "bsName": batSecondName,
+                "bsID": batSecondID,
+                "bfRuns": info['bFRuns'],
+                "bfWickets": info['bFWickets'],
+                "bfOvers": info['bFOvers'],
+                "bfCRR": bfCRR,
+                "bsRuns": info['bSRuns'],
+                "bsWickets": info['bSWickets'],
+                "bsOvers": info['bSOvers'],
+                "bsCRR": bsCRR,
+                "bsRRR": bsRRR,
+                "overResults": info['overResults'],
+                "ballEvent": info['ballEvent'],
+                "ballText": info['ballText'],
+                "currentOver": info['currentOver'],
+                "currentBall": info['currentBall'],
+                "strikeID": info['batter1ID'],
+                "strikeFName": info['batter1FName'],
+                "strikeLName": info['batter1LName'],
+                "strikeRuns": info['batter1Runs'],
+                "strikeBalls": info['batter1Balls'],
+                "strikeFours": info['batter1Fours'],
+                "strikeSixes": info['batter1Sixes'],
+                "strikeSR": info['batter1SR'],
+                "nstrikeID": info['batter2ID'],
+                "nstrikeFName": info['batter2FName'],
+                "nstrikeLName": info['batter2LName'],
+                "nstrikeRuns": info['batter2Runs'],
+                "nstrikeBalls": info['batter2Balls'],
+                "nstrikeFours": info['batter2Fours'],
+                "nstrikeSixes": info['batter2Sixes'],
+                "nstrikeSR": info['batter2SR'],
+                "sbowlID": info['bowler1ID'],
+                "sbowlFName": info['bowler1FName'],
+                "sbowlLName": info['bowler1LName'],
+                "sbowlOvers": info['bowler1Overs'],
+                "sbowlMaidens": info['bowler1Maidens'],
+                "sbowlRuns": info['bowler1Runs'],
+                "sbowlWickets": info['bowler1Wickets'],
+                "sbowlEcon": info['bowler1Econ'],
+                "nsbowlID": info['bowler2ID'],
+                "nsbowlFName": info['bowler2FName'],
+                "nsbowlLName": info['bowler2LName'],
+                "nsbowlOvers": info['bowler2Overs'],
+                "nsbowlMaidens": info['bowler2Maidens'],
+                "nsbowlRuns": info['bowler2Runs'],
+                "nsbowlWickets": info['bowler2Wickets'],
+                "nsbowlEcon": info['bowler2Econ'],
+                "lBat": info['lastBat'],
+                "fbattingCard": json.loads(info['fbattingCard']),
+                "fyetToBat": json.loads(info['fyetToBat']),
+                "fbowlingCard": json.loads(info['fbowlingCard']),
+                "sbattingCard": sbattingCard,
+                "syetToBat": syetToBat,
+                "sbowlingCard": sbowlingCard,
+                "log": logText
+            }
     else:
         liveInfo = {"status": "No Match Found"}
     # print(liveInfo)
